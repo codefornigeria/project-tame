992299ecaad320a339b1b931c4c306553cb3fcd5
omofolarin <folarinshonibare@gmail.com>
2017-04-19 00:43:08 +0100

    separated development folder from production folder, adjusted header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    clean = require('gulp-clean'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
     less = require('gulp-less'),
@@ -13,110 +12,116 @@ var gulp = require('gulp'),
     gulpNgConfig = require('gulp-ng-config'),
     filter = require('gulp-filter'),
     json = JSON.parse(fs.readFileSync('./package.json')),
-	purify = require('gulp-purifycss');
+    del = require('del'),
+    vinylPaths = require('vinyl-paths'),
+    purify = require('gulp-purifycss');
 
 
-gulp.task('clean', function () {
-  return gulp.src('dist/**/*')
-    .pipe(filter(['*', '!.git']))
-    .pipe(clean());
+gulp.task('clean:production', function() {
+    return gulp.src('production/*')
+        .pipe(vinylPaths(del))
+        .pipe(filter(['*', '!.git']))
+        .pipe(gulp.dest('./production'));
 });
 
-gulp.task('config', function () {
-  gulp.src('./local.json')
-  .pipe(gulpNgConfig('app.config', {
-    environment: argv.production ? 'production' : 'development'
-  }))
-  .pipe(gulp.dest('app'))
+gulp.task('config', function() {
+    gulp.src('./local.json')
+        .pipe(gulpNgConfig('app.config', {
+            environment: argv.production ? 'production' : 'development'
+        }))
+        .pipe(gulp.dest('./configs/'))
 })
 
-gulp.task('connect', function(){
+gulp.task('copy-html', ['clean:production'], function() {
+    return gulp.src(['./configs/index.html', './development/**/*.html', '!node_modules/**/*.html', '!./development/vendor/**/*.html', '!production/**/*.html', '!static/**/*.html'])
+               .pipe(gulp.dest('./production'));
+});
+
+gulp.task('copy-fonts', ['clean:production'], function(){
+    return gulp.src('./development/assets/fonts/**/*')
+               .pipe(gulp.dest('./production/assets/fonts'));
+});
+
+gulp.task('copy-images', ['clean:production'], function(){
+    return gulp.src('./development/assets/img/**/*')
+               .pipe(gulp.dest('./production/assets/img'));
+});
+
+gulp.task('vendorcss', ['clean:production'], function(){
+    return gulp
+			.src([
+        './development/assets/vendor/animate/animate.css',
+        './development/assets/vendor/bootstraps/bootstraps.css',
+        './development/assets/vendor/font-awesome/font-awesome.css',
+        './developmentassets/vendor/angular-carousel/dist/angular-carousel.css',
+				'./development/assets/vendor/**/*.css'
+			])
+		  .pipe(concat('vendor.css'))
+		  .pipe(gulpif(argv.production, cssmin()))
+			.pipe(gulpif(argv.production, purify(['./production/**/*.js', './production/**/*.html'])))
+		    .pipe(gulp.dest('production/assets/css'))
+});
+
+gulp.task('vendorjs', ['clean:production'], function(){
+    return gulp
+		.src([
+      './development/assets/vendor/angular/angular.js',
+      './development/assets/vendor/angular/angular-animate/angular-animate.js',
+      './development/assets/vendor/angular/angular-bootstrap/ui-bootstrap-custom-tpls-0.14.3.min.js',
+      './development/assets/vendor/angular/angular-ui-router/angular-ui-router.js',
+      './development/assets/vendor/angular/restangular/restangular.min.js',
+      './development/assets/vendor/angular/ng-feathers.standalone.min.js',
+      './development/assets/vendor/angular/underscore/underscore-min.js',
+      './development/assets/vendor/angular/angular-chart/Chart.js',
+      './development/assets/vendor/angular/angular-chart/angular-chart.min.js',
+      './development/assets/vendor/angular/dirDisqus.js',
+    './development/assets/vendor/angular/angular-sanitize.min.js',
+    './development/assets/vendor/angular/tv4.js',
+    './development/assets/vendor/angular/tv4.async-jquery.js',
+    './development/assets/vendor/angular/ObjectPath.js',
+    './development/assets/vendor/angular/schema-form.min.js',
+    './development/assets/vendor/angular/dist/bootstrap-decorator.min.js',
+    './development/assets/vendor/angular/angular-carousel/dist/angular-carousel.js',
+		'./development/assets/vendor/*.js',
+    './development/assets/vendor/bootstrap/bootstrap.min.js',
+    './development/assets/vendor/bootstrap/jquery.min.js',
+    './developmentassets/vendor/bootstrap/main.js'
+		])
+    .pipe(concat('vendor.js'))
+    .pipe(gulpif(argv.production, uglify()))
+    .pipe(gulp.dest('./production/assets/js'));
+});
+
+gulp.task('appjsfiles', ['clean:production'], function(){
+    return gulp.src([ './configs/local.js', './development/assets/js/app.js', './development/assets/js/*.js', './development/modules/**/*.js'])
+    .pipe(concat(json.name.toLowerCase() + '.js'))
+    .pipe(gulpif(argv.production, uglify()))
+    .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+    .pipe(gulp.dest('./production/assets/js'));
+});
+
+gulp.task('connect', function() {
     connect.server({
-        root: 'dist',
+        root: './production/',
         port: 4930
     });
 });
 
-gulp.task('copy-html', ['clean'], function(){
-    return gulp.src(['app/index.html', './**/*.html', '!node_modules/**/*.html', '!vendor/**/*.html', '!dist/**/*.html', '!static/**/*.html'])
-            .pipe(gulp.dest('dist'));
-});
-
-gulp.task('copy-fonts', ['clean'], function(){
-    return gulp.src('assets/fonts/**/*')
-            .pipe(gulp.dest('dist/assets/fonts'));
-});
-
-gulp.task('copy-images', ['clean'], function(){
-    return gulp.src('assets/img/**/*')
-            .pipe(gulp.dest('dist/assets/img'));
-});
-
-gulp.task('vendorjs', ['clean'], function(){
-    return gulp
-		.src([
-      'assets/vendor/angular/angular.js',
-      'assets/vendor/angular/angular-animate/angular-animate.js',
-      'assets/vendor/angular/angular-bootstrap/ui-bootstrap-custom-tpls-0.14.3.min.js',
-      'assets/vendor/angular/angular-ui-router/angular-ui-router.js',
-      'assets/vendor/angular/restangular/restangular.min.js',
-      'assets/vendor/angular/ng-feathers.standalone.min.js',
-      'assets/vendor/angular/underscore/underscore-min.js',
-      'assets/vendor/angular/angular-chart/Chart.js',
-      'assets/vendor/angular/angular-chart/angular-chart.min.js',
-      'assets/vendor/angular/dirDisqus.js',
-    'assets/vendor/angular/angular-sanitize.min.js',
-    'assets/vendor/angular/tv4.js',
-    'assets/vendor/angular/tv4.async-jquery.js',
-    'assets/vendor/angular/ObjectPath.js',
-    'assets/vendor/angular/schema-form.min.js',
-    'assets/vendor/angular/dist/bootstrap-decorator.min.js',
-    'assets/vendor/angular/angular-carousel/dist/angular-carousel.js',
-		'assets/vendor/*.js'
-		])
-        .pipe(concat('vendor.js'))
-        .pipe(gulpif(argv.production, uglify()))
-        .pipe(gulp.dest('dist/assets/js'));
-});
-
-gulp.task('vendorcss', ['clean'], function(){
-    return gulp
-			.src([
-        'assets/vendor/animate/animate.css',
-        'assets/vendor/bootstraps/bootstraps.css',
-        'assets/vendor/font-awesome/font-awesome.css',
-        'assets/vendor/angular-carousel/dist/angular-carousel.css',
-				'assets/vendor/**/*.css'
-			])
-		    .pipe(concat('vendor.css'))
-		    .pipe(gulpif(argv.production, cssmin()))
-			.pipe(gulpif(argv.production, purify(['./dist/**/*.js', './dist/**/*.html'])))
-		    .pipe(gulp.dest('dist/assets/css'))
-});
-
-gulp.task('appjsfiles', ['clean'], function(){
-    return gulp.src([ 'app/local.js', 'assets/js/app.js', 'assets/js/*.js', 'modules/**/*.js'])
-            .pipe(concat(json.name.toLowerCase() + '.js'))
-            .pipe(gulpif(argv.production, uglify()))
-            .pipe(gulpif(argv.production, rename({suffix: '.min'})))
-            .pipe(gulp.dest('dist/assets/js'));
-});
-
 gulp.task('link-files', ['copy-images', 'copy-fonts', 'copy-html',  'vendorjs', 'vendorcss', 'appjsfiles'], function () {
-  var target = gulp.src('dist/index.html');
-  var sources = gulp.src(['dist/assets/js/vendor.js', 'dist/assets/js/*.js', 'dist/assets/css/vendor.css', 'dist/assets/css/*.css'], {read: false});
+  var target = gulp.src('./production/index.html');
+  var sources = gulp.src(['./production/assets/js/vendor.js', './production/assets/js/*.js', './production/assets/css/vendor.css', './production/assets/css/*.css'], {read: false});
   return target.pipe(inject(sources, {relative: true}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./production'));
 });
 
-gulp.task('style', ['clean'], function(){
-    return gulp.src(['assets/css/app.css', 'assets/css/ted.css'])
+gulp.task('style', ['clean:production'], function(){
+    return gulp.src(['./development/assets/css/app.css', '.development/assets/css/ted.css'])
 
         .pipe(concat(json.name.toLowerCase() + '.css'))
         .pipe(gulpif(argv.production, cssmin()))
-		.pipe(gulpif(argv.production, purify(['./dist/**/*.js', './dist/**/*.html'])))
+		.pipe(gulpif(argv.production, purify(['./production/assets/**/*.js', './production/modules/**/*.html'])))
         .pipe(gulpif(argv.production, rename({suffix: '.min'})))
-		.pipe(gulp.dest('dist/assets/css'))
+		.pipe(gulp.dest('./production/assets/css'))
 });
 
 gulp.task('logger', function(){
@@ -125,6 +130,9 @@ gulp.task('logger', function(){
 
 gulp.task('build', [ 'config', 'style', 'link-files']);
 
+
+gulp.task('default', ['clean:production', 'config', 'copy-images', 'connect', 'copy-html', 'vendorjs','vendorcss','appjsfiles','copy-fonts','link-files']);
+
 gulp.task('serve', ['connect', 'config', 'style', 'link-files'], function () {
-	return gulp.watch(['app/**/*', 'assets/**/*', 'modules/**/*'], ['logger', 'style', 'link-files']);
+	return gulp.watch(['./configs/**/*', './development/assets/**/*', './development/modules/**/*'], ['logger','style', 'link-files']);
 });
