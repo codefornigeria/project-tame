73af196a89b911cea44a122c945a2e0e96ca3763
adeb6600 <ini@mustardlabs.io>
2017-07-11 10:43:34 +0100

    added result to request process

--- development/assets/js/controllers/requestResultController.js
+++ development/assets/js/controllers/requestResultController.js
@@ -2,7 +2,7 @@ angular.module('app.controllers')
   .controller('requestResultCtrl', function (requestStatus,
     $scope, $rootScope, $state, $stateParams,
     $feathers, $auth, AuthService, LocalService, $anchorScroll, $location, toastr) {
-
+      $scope.requestData =requestStatus
     console.log($stateParams)
   
         
