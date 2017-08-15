693842b06a167db66b64585c78446b5c2c53677e
inioluwa adebanjo <adeb6600@gmail.com>
2017-08-15 13:51:55 +0100

    updates

--- production/assets/js/tame.js
+++ production/assets/js/tame.js
@@ -1183,58 +1183,6 @@ angular.module('app.directives', [])
 	}
 })
 
-angular.module('app.filters', [])
-
-    .filter('asessorType', function () {
-        return function (assessor) {
-            var txt = 'Public Assessment';
-            switch (assessor) {
-                case 'public-assessor':
-                    txt = 'Public Assessment'
-                    break;
-                case 'self-assessor':
-                    txt = 'Self Assessment'
-                    break;
-                case 'independent-assessor':
-                    txt = 'Independent Assessment'
-                    break;
-            }
-            return txt;
-        };
-    })
-    .filter('countAntidotes', function () {
-        return function (data) {
-            var antidoteCount = 0
-            data.map(rating => {
-                rating.antidotes.map(antidote => {
-                    antidoteCount++
-                })
-            })
-
-            return antidoteCount
-
-        };
-    })
-    .filter('scoreFilter', function () {
-        return function (data) {
-            console.log('the data valuer',data)
-            if (data <= 0.49) {
-                return 'RED'
-            }
-            else if (data > 0.49 && data <= 0.69) {
-                return 'AMBER RED'
-            }else   if(data ==0.70) {
-          return 'AMBER'
-      } else if (data >= 0.71 && data <=0.84) {
-                return 'AMBER GREEN'
-            } else if (data >= 0.85) {
-                return ' GREEN'
-            }
-        }
-        
-    })
-
-
   angular.module('app.services', [])
   .factory('AuthService', function($q, $feathers, $rootScope){
   return {
